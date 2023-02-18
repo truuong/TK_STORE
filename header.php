c69105b56c3e48f8c5c7fe11ce2297f8b0f36da8
truuong <111691959+truuong@users.noreply.github.com>
2023-02-18 10:50:18 +0700

    edit

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-  <!-- Required meta tags -->
+  <!-- Required meta tags  status-->
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <link rel="icon" href="img/favicon.png" type="image/png" />
